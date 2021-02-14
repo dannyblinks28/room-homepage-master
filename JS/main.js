71d11c9ec1f4a294dac6544166ef154437cef04e
Daniel Nsirim <nsirimdanielazeru@gmail.com>
2021-02-14 12:30:52 +0100

    first commit

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+
+document.getElementById("mobile-cta").onclick = function () {
+    document.getElementById("nav-menu-show").style.display = "block";
+}
+
+document.getElementById("mobile-exit").onclick = function () {
+    document.getElementById("nav-menu-show").style.display = "none";
+
+} 
